57552b856bc2e4c770f5c4fb6a9292a214ad2ca9
Jugraj Singh <jsbains96@gmail.com>
2023-08-02 08:12:20 +0100

    added query.selectors and created a function for number to be shown in console.log when numbers are pressed on screen

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
 import "./style.scss";
+
+const screenDisplay = document.querySelector(
+  ".calculator__display"
+) as HTMLElement;
+const numbers = document.querySelectorAll(".number");
+const operator = document.querySelectorAll(".operator");
+const allClear = document.querySelector(".data-all-clear");
+const equals = document.querySelector(
+  ".calculator__buttons--actions.operator.data-equals"
+);
+
+let firstNumber: string = "";
+let calculatorOperator: string = "";
+let secondNumber: string = "";
+
+const handleNumberPress = (event: any) => {
+  const target = event.target;
+  const digit = target.innerText;
+  if (calculatorOperator) {
+    secondNumber += digit;
+  } else {
+    firstNumber += digit;
+  }
+  updateHTML();
+};
+
+// EVENT LISTENERS
+
+numbers.forEach((number) => {
+  number.addEventListener("click", handleNumberPress);
+});
+
